78d0adc88d7bcb71e057698f3eae3a0a4ece7c70
kiwikigame <jejebainier@gmail.com>
2022-08-19 00:07:23 +0200

    New

---
+++ main.py
@@ -0,0 +1,35 @@
+import requests, random, re, time
+
+url = "aaa"
+while(url[0:4] != "http"):
+    url = input("URL : ")
+    if(url[0:4] != "http"):
+        print("L'url doit commencer par http...")
+    else:
+
+        url = f"{url}/"
+        psg = input("Nombre de scan (0=inf) -Défaut=0 : ")
+        if not psg:
+            psg = 0
+        psg = int(psg)
+        delay = input("Delay entre chaque fichier (en sec) -Défaut=0 : ")
+        if not delay:
+            delay = 0
+        delay = int(delay)
+        iter = 1
+        if psg == 0:
+            print(f"Telechargement de tout les fichier depuis {url} avec une infinité d'itérations et un delay de {delay} secondes")
+        else:
+            print(f"Telechargement de tout les fichier depuis {url} avec {psg} itérations et un delay de {delay} secondes")
+        while iter <= psg or psg == 0:
+            time.sleep(delay)
+            iter += 1
+            headers = {"User-Agent": f"Mozilla/5.0 (X11; Windows x86_64; rv:60.0) Gecko/{random.randint(12548,1354795)} Firefox/60.0",
+                       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                       "Accept-Language": "fr-FR,fr;q=0.9"
+                      }
+            start = time.time()
+            response = requests.get(url=url, headers=headers)
+            name = re.search('filename="(.*?)"',str(response.headers)).group(1)
+            open(f"img/{name[:-4]}.{name[-3:]}", "wb").write(response.content)
+            print(f"{name}   {time.time()-start:.3}ms   {response.content.__sizeof__()}ko")
