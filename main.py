3fe73c70bc4a284cb3e692faecd250e90fc2a5ae
kiwikigame <jejebainier@gmail.com>
2022-08-19 00:44:32 +0200

    add file dir

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import requests, random, re, time
+import requests, random, re, time, os
 
 url = "aaa"
+
 while(url[0:4] != "http"):
     url = input("URL : ")
     if(url[0:4] != "http"):
         print("L'url doit commencer par http...")
     else:
-
         url = f"{url}/"
         psg = input("Nombre de scan (0=inf) -Défaut=0 : ")
         if not psg:
@@ -17,10 +17,18 @@ while(url[0:4] != "http"):
             delay = 0
         delay = int(delay)
         iter = 1
+
         if psg == 0:
             print(f"Telechargement de tout les fichier depuis {url} avec une infinité d'itérations et un delay de {delay} secondes")
         else:
             print(f"Telechargement de tout les fichier depuis {url} avec {psg} itérations et un delay de {delay} secondes")
+
+        if not os.path.exists('Out'):
+            os.makedirs('Out')
+        globalname = re.search('\:\/\/(.*?)\.',str(url)).group(1)
+        if not os.path.exists(f"Out/{globalname}"):
+            os.makedirs(f"Out/{globalname}")
+        
         while iter <= psg or psg == 0:
             time.sleep(delay)
             iter += 1
@@ -31,5 +39,5 @@ while(url[0:4] != "http"):
             start = time.time()
             response = requests.get(url=url, headers=headers)
             name = re.search('filename="(.*?)"',str(response.headers)).group(1)
-            open(f"img/{name[:-4]}.{name[-3:]}", "wb").write(response.content)
-            print(f"{name}   {time.time()-start:.3}ms   {response.content.__sizeof__()}ko")
+            open(f"Out/{globalname}/{name[:-4]}.{name[-3:]}", "wb").write(response.content)
+            print(f"{name}   {time.time()-start:.3}ms   {response.content.__sizeof__()/1000}ko")
